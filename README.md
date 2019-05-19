fddde359d385536d4575e11d1773a6e7a4a0d15a
lixujia <lixujia.cn@gmail.com>
2019-05-19 11:33:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Payload Jsonschema Validator
+## Requirements
+  - django
+  - djangorestframework
+  - jsonschema
+  
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="DrfRequestJsonValidator",
+    version='0.0.1',
+    author='Li Xujia',
+    author_email='lixujia.cn@gmail.com',
+    description="A package use for validate json payload with jsonschema",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=['setup.py', 'requirements.txt']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
